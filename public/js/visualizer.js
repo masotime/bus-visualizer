fecd58b0221c5a17dfc237bbc20bbafb88cd2dcc
Benjamin Goh <bengoh@paypal.com>
2014-04-17 18:00:37 -0700

    Fixed performance issue as much as I can

--- public/js/visualizer.js
+++ public/js/visualizer.js
@@ -1,62 +1,58 @@
 var width = 960, height = 1160;
-var svg = d3.select('div#visual-container').append('svg').attr('width', width).attr('height', height);
-var pathCache = {};
 
-var renderGEOJson = function(className) {
+var RendererFactory = (function(selector) {
 
-	if (!className) {
-		throw new Error("Specify a valid classname to render these paths");
-	}
+	var svg = d3.select('div#visual-container').append('svg').attr('width', width).attr('height', height),
+		pathCache = {},
+		previousBounds;
 
-	function generatePath() {
-		// based upon cached paths, calculate global bounds and determine a universal
-		// projection for all of them
-		var allfeatures = [];
+	var renderGEOJson = function(className) {
 
-		for (key in pathCache) {
-			allfeatures = allfeatures.concat(pathCache[key].features);
+		if (!className) {
+			throw new Error("Specify a valid classname to render these paths");
 		}
 
-		// lots of references to http://stackoverflow.com/a/14691788
-	 	var projection = d3.geo.albers().scale(1).translate([0, 0]);
-		var path = d3.geo.path().projection(projection);
-		var bounds = path.bounds({type: "FeatureCollection", features: allfeatures});
+		var calculateGlobalBounds = function() {
+			// based upon cached paths, calculate global bounds and determine a universal
+			// projection for all of them
+			var allfeatures = [];
 
-		// based on the bounds, create a new projection
-		// console.log('world bounds = ',JSON.stringify(bounds,null,4));
+			for (key in pathCache) {
+				allfeatures = allfeatures.concat(pathCache[key].features);
+			}
 
-		// 1. scale must match the larger of (x2-x1)/width, (y2-y1)/height
-		var x1 = bounds[0][0];
-		var x2 = bounds[1][0];
-		var y1 = bounds[0][1];
-		var y2 = bounds[1][1];
+			// lots of references to http://stackoverflow.com/a/14691788
+		 	var projection = d3.geo.albers().scale(1).translate([0, 0]);
+			var path = d3.geo.path().projection(projection);
+			var bounds = path.bounds({type: "FeatureCollection", features: allfeatures});
 
-		var s = 1.1/Math.max((x2-x1)/width, (y2-y1)/height);
-		var t = [(width - s * (x1+x2))/2, (height - s * (y1+y2))/2];
+			return bounds;
 
-		// update the projection
-		projection.scale(s).translate(t);
+		}
 
-		// smaller pixel size for points
-		path.pointRadius(3);
+		var generatePath = function() {
+			var globalBounds = calculateGlobalBounds();
 
-		// return the path
-		return path;
-	}
+			// lots of references to http://stackoverflow.com/a/14691788
+			// 1. scale must match the larger of (x2-x1)/width, (y2-y1)/height
+			var x1 = globalBounds[0][0];
+			var x2 = globalBounds[1][0];
+			var y1 = globalBounds[0][1];
+			var y2 = globalBounds[1][1];
 
+			var s = 1.1/Math.max((x2-x1)/width, (y2-y1)/height);
+			var t = [(width - s * (x1+x2))/2, (height - s * (y1+y2))/2];
 
-	return function(err, featureCollection) {
-		// store the paths inside the cache
-		pathCache[className] = featureCollection;
+			// create a new projection and path to use
+		 	var projection = d3.geo.albers().scale(s).translate(t);
+			var path = d3.geo.path().projection(projection);
+			path.pointRadius(3);
 
-		// generate a path with global bounds
-		var path = generatePath();
+			// return the path
+			return path;
+		}
 
-		// the path will automatically use the new projection
-		// console.log('scaled bounds = ',JSON.stringify(path.bounds(featureCollection), null, 4));
-
-		// rerender all paths in the cache
-		for (key in pathCache) {
+		var renderPath = function(key, path) {
 			svg.selectAll('path.'+key).remove();
 			svg.selectAll('path.'+key)
 				.data(pathCache[key].features)
@@ -65,11 +61,62 @@ var renderGEOJson = function(className) {
 					.attr('class', key);
 		}
 
+
+		return function(err, featureCollection) {
+			// store the paths inside the cache
+			pathCache[className] = featureCollection;
+
+			// generate a path with global bounds
+			var path = generatePath();
+
+			// calculate the current bounds
+			var currentBounds = calculateGlobalBounds();
+			var refreshAll = false;
+
+			// compare against the previous bounds
+			if (previousBounds) {
+				var lowerBoundsSame = previousBounds[0][0] === currentBounds[0][0] && previousBounds[0][1] === currentBounds[0][1];
+				var upperBoundsSame = previousBounds[1][0] === currentBounds[1][0] && previousBounds[1][1] === currentBounds[1][1];
+				if (lowerBoundsSame && upperBoundsSame) {
+					console.log('Full refresh NOT needed');
+				} else {
+					console.log('Full refresh needed');
+					if (!lowerBoundsSame) {
+						console.log('Lower bounds do not match.',previousBounds[0],'vs',currentBounds[0]);
+					} else {
+						console.log('Upper bounds do not match.',previousBounds[1],'vs',currentBounds[1]);
+					}
+					refreshAll = true;
+				}
+			}
+
+			// update previousBounds
+			previousBounds = currentBounds;
+
+			// the path will automatically use the new projection
+			// console.log('scaled bounds = ',JSON.stringify(path.bounds(featureCollection), null, 4));
+
+			if (refreshAll) {
+				// rerender all paths in the cache
+				for (key in pathCache) {
+					renderPath(key, path);
+				}
+			} else {
+				renderPath(className, path);
+			};
+
+		}
+	};
+
+	return {
+		'render': renderGEOJson
 	}
-};
+});
+
+var renderer = RendererFactory('div#visual-container');
 
 // heavy references to https://github.com/mbostock/d3/wiki/Geo-Paths
-d3.json('/sfmaps/streets.json', renderGEOJson('streets'));
+d3.json('/sfmaps/streets.json', renderer.render('streets'));
 //d3.json('/sfmaps/arteries.json', renderGEOJson('arteries'));
 //d3.json('/sfmaps/freeways.json', renderGEOJson('freeways'));
 //d3.json('/sfmaps/neighborhoods.json', renderGEOJson('neighborhoods'));
@@ -151,7 +198,7 @@ var Timer = (function() {
 				// check the status. if it signals a stop, then don't try and render and reschedule
 				if (threadStatus[id]) {
 					// render on the canvas now
-					renderGEOJson('buses')(null, {
+					renderer.render('buses')(null, {
 						type: "FeatureCollection",
 						features: features
 					});
